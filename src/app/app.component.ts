5eaf9189ae22fbbeafd1738f9e998d77364823de
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-08-20 10:41:35 +0300

    Initialize new app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'color-scheme-editor';
+}
