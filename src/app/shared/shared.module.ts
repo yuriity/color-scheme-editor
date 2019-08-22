e9c76f9f6216dbe9c467f2f0d74f9d442b40165e
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-08-22 18:35:41 +0300

    Add Header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material';
+
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [HeaderComponent],
+  imports: [CommonModule, MatToolbarModule],
+  exports: [CommonModule, MatToolbarModule, HeaderComponent]
 })
 export class SharedModule {}
