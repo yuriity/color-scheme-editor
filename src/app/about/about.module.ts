bb2f4a011382fccb0c7bf6883fd535a5acf78221
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-08-22 18:32:50 +0300

    Add About component

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SharedModule } from '@app/shared';
+
+import { AboutComponent } from './about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [SharedModule, RouterModule.forChild(routes)]
+})
+export class AboutModule {}
