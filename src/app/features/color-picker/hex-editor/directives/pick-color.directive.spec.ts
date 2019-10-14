b2efab9cce898d3b63e2c02c198f96eb1d065aef
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-10-14 13:00:36 +0300

    Optimize PickColorDirective

--- src/app/features/color-picker/hex-editor/directives/pick-color.directive.spec.ts
+++ src/app/features/color-picker/hex-editor/directives/pick-color.directive.spec.ts
@@ -1,8 +1,43 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { PickColorDirective } from './pick-color.directive';
 
+@Component({
+  template: `
+    <canvas csePickColor (colorPicked)="onColorPicked($event)"></canvas>
+  `
+})
+class ColorPickedTestComponent {
+  onColorPicked(event: MouseEvent) {}
+}
+
 describe('PickColorDirective', () => {
+  let component: ColorPickedTestComponent;
+  let fixture: ComponentFixture<ColorPickedTestComponent>;
+  let inputEl: DebugElement;
+  let colorPicked: PickColorDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ColorPickedTestComponent, PickColorDirective]
+    });
+    fixture = TestBed.createComponent(ColorPickedTestComponent);
+    fixture.detectChanges();
+
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('canvas'));
+    colorPicked = fixture.debugElement
+      .query(By.directive(PickColorDirective))!
+      .injector.get<PickColorDirective>(PickColorDirective);
+  });
+
   it('should create an instance', () => {
-    const directive = new PickColorDirective();
-    expect(directive).toBeTruthy();
+    console.log('should create an instance');
+    inputEl.triggerEventHandler('mousedown', { buttons: 0, offsetX: 2, offsetY: 3 });
+    fixture.detectChanges();
+
+    expect(colorPicked).toBeTruthy();
   });
 });
--- src/app/features/color-picker/hex-editor/directives/pick-color.directive.ts
+++ src/app/features/color-picker/hex-editor/directives/pick-color.directive.ts
@@ -1,4 +1,17 @@
-import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
+import {
+  Directive,
+  HostListener,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  OnDestroy,
+  NgZone,
+  ElementRef,
+  OnInit
+} from '@angular/core';
+import { Subject, fromEvent, Observable } from 'rxjs';
+import { Platform } from '@angular/cdk/platform';
+import { auditTime, takeUntil, mergeMap, tap, filter } from 'rxjs/operators';
 
 export interface Point {
   readonly x: number;
@@ -8,48 +21,67 @@ export interface Point {
 @Directive({
   selector: '[csePickColor]'
 })
-export class PickColorDirective {
-  private mouseDown = false;
-  private offsetX: number;
-  private offsetY: number;
-  private height: number;
-  private width: number;
+export class PickColorDirective implements OnInit, OnDestroy {
+  private oldPoint: Point;
+  private readonly destroyed$ = new Subject<void>();
+
+  private mousemove$: Observable<MouseEvent>;
+  private mousedown$: Observable<MouseEvent>;
+  private mouseup$: Observable<Event>;
 
   @Output() colorPicked = new EventEmitter<Point>();
 
-  constructor() { }
+  constructor(private elementRef: ElementRef<HTMLCanvasElement>, private zone: NgZone) {}
 
-  @HostListener('mousedown', ['$event'])
-  onMouseDown($event: MouseEvent) {
-    if ($event.buttons > 1) { return; }
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      this.mousemove$ = fromEvent<MouseEvent>(window, 'mousemove');
+      this.mousedown$ = fromEvent<MouseEvent>(this.elementRef.nativeElement, 'mousedown');
+      this.mouseup$ = fromEvent(window, 'mouseup');
 
-    const canvas = $event.target as HTMLCanvasElement;
-    this.offsetX = canvas.getBoundingClientRect().left;
-    this.offsetY = canvas.getBoundingClientRect().top;
-    this.height = canvas.height;
-    this.width = canvas.width;
-
-    this.colorPicked.emit({
-      x: $event.offsetX,
-      y: $event.offsetY
+      this.mousedown$
+        .pipe(
+          takeUntil(this.destroyed$),
+          filter(event => event.buttons === 1),
+          tap(event => {
+            this.onColorPicked({
+              x: event.offsetX,
+              y: event.offsetY
+            });
+          }),
+          mergeMap(_ =>
+            this.mousemove$.pipe(
+              takeUntil(this.mouseup$),
+              auditTime(16)
+            )
+          )
+        )
+        .subscribe(event => this.onWindowMouseMove(event));
     });
-
-    this.mouseDown = true;
   }
 
-  @HostListener('window:mousemove', ['$event'])
-  onWindowMouseMove($event: MouseEvent) {
-    if (this.mouseDown) {
-      this.colorPicked.emit({
-        x: this.limitValue(Math.round($event.x - this.offsetX), 0, this.width),
-        y: this.limitValue(Math.round($event.y - this.offsetY), 0, this.height)
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  private onWindowMouseMove(mouseEvent: MouseEvent) {
+    const clientRect = this.elementRef.nativeElement.getBoundingClientRect();
+
+    this.onColorPicked({
+      x: this.limitValue(Math.round(mouseEvent.x - clientRect.left), 0, clientRect.width),
+      y: this.limitValue(Math.round(mouseEvent.y - clientRect.top), 0, clientRect.height)
+    });
+  }
+
+  private onColorPicked(point: Point) {
+    if (!this.oldPoint || this.oldPoint.x !== point.x || this.oldPoint.y !== point.y) {
+      console.log('onColorPicked', point);
+      this.zone.run(() => {
+        this.oldPoint = point;
+        this.colorPicked.emit(point);
       });
     }
-  }
-
-  @HostListener('window:mouseup')
-  onMouseUp() {
-    this.mouseDown = false;
   }
 
   private limitValue(value: number, min: number, max: number): number {
