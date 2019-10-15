104192ad347afbd527d2f554e2e334fcef0ca1cb
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-10-15 11:09:21 +0300

    Fix sm layout for 'edit-token' page

--- src/app/features/edit-token-color/edit-token-color.component.ts
+++ src/app/features/edit-token-color/edit-token-color.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import * as tinycolor from 'tinycolor2';
 
 import { AppState } from 'app/core/store/core.state';
-import { TinycolorInstance } from 'app/core/models/tinycolor-instance';
 import { selectSelectedTokenResource } from 'app/core/store/tokens/tokens.selectors';
 import { TokenColorResource } from 'app/core/models/token-color.resource';
 import { Update } from '@ngrx/entity';
